f85374eab96cf0472ab56f6633bbe2d398df10a4
Gokul <gokulchandan24@gmail.com>
2025-10-05 00:47:53 +0530

    a

--- src/utils/functions/metadata.ts
+++ src/utils/functions/metadata.ts
@@ -1,48 +1,41 @@
 import { Metadata } from "next";
 
+interface MetadataOptions {
+  title?: string;
+  description?: string;
+  image?: string | null;
+  icons?: Metadata["icons"];
+  noIndex?: boolean;
+}
+
 export const generateMetadata = ({
-    title = `${process.env.NEXT_PUBLIC_APP_NAME} - The Link Management Platform for Businesses`,
-    description = `${process.env.NEXT_PUBLIC_APP_NAME} is the link management platform for businesses. It helps you build, brand, and track your links.`,
-    image = "/thumbnail.png",
-    icons = [
-        {
-            rel: "apple-touch-icon",
-            sizes: "32x32",
-            url: "/apple-touch-icon.png"
-        },
-        {
-            rel: "icon",
-            sizes: "32x32",
-            url: "/favicon-32x32.png"
-        },
-        {
-            rel: "icon",
-            sizes: "16x16",
-            url: "/favicon-16x16.png"
-        },
-    ],
-    noIndex = false
-}: {
-    title?: string;
-    description?: string;
-    image?: string | null;
-    icons?: Metadata["icons"];
-    noIndex?: boolean;
-} = {}): Metadata => ({
+  title = `${process.env.NEXT_PUBLIC_APP_NAME} - OptimAIzer | AI-Powered Link Management Platform`,
+  description = `${process.env.NEXT_PUBLIC_APP_NAME} is OptimAIzer, the AI-powered platform to manage, brand, and track your links effortlessly.`,
+  image = "/thumbnail.png",
+  icons = [
+    { rel: "apple-touch-icon", sizes: "32x32", url: "/apple-touch-icon.png" },
+    { rel: "icon", sizes: "32x32", url: "/favicon-32x32.png" },
+    { rel: "icon", sizes: "16x16", url: "/favicon-16x16.png" },
+  ],
+  noIndex = false,
+}: MetadataOptions = {}): Metadata => {
+  return {
     title,
     description,
     icons,
     openGraph: {
-        title,
-        description,
-        ...(image && { images: [{ url: image }] }),
+      title,
+      description,
+      ...(image ? { images: [{ url: image }] } : {}),
     },
     twitter: {
-        title,
-        description,
-        ...(image && { card: "summary_large_image", images: [image] }),
-        creator: "@shreyassihasane",
+      title,
+      description,
+      ...(image
+        ? { card: "summary_large_image", images: [image] }
+        : { card: "summary" }),
+      creator: "@OptimAIzerHQ",
     },
-    // metadataBase: new URL(process.env.APP_DOMAIN!),
-    ...(noIndex && { robots: { index: false, follow: false } }),
-});
+    ...(noIndex ? { robots: { index: false, follow: false } } : {}),
+  };
+};
